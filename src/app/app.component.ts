cb231da62b2dae0147a225860d02f1b80df55131
Emily Sauer <Emily Sauer>
2017-01-27 11:38:18 -0500

    Simple Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { MyTeamsPage } from './pages/pages';
 
 
 @Component({
@@ -12,18 +11,12 @@ import { Page2 } from '../pages/page2/page2';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
-
-  pages: Array<{title: string, component: any}>;
+  rootPage: any = MyTeamsPage;
 
   constructor(public platform: Platform) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
-    ];
 
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { GamePage, MyTeamsPage, TeamDetailPage, TeamsPage, TournamentsPage } from '../pages/pages';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    MyTeamsPage,
+    GamePage,
+    TeamsPage,
+    TeamDetailPage,
+    TournamentsPage
+    
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +20,12 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    MyTeamsPage,
+    GamePage,
+    TeamsPage,
+    TeamDetailPage,
+    TournamentsPage
+
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
